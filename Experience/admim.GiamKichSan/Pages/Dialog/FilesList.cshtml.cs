b13ab3efe17d1aa5d611da32b792915ae417a119
LDAP\vcduan <duanvc2811@gmail.com>
2023-01-19 15:47:58 +0700

    add danh sách

---
+++ Experience/admim.GiamKichSan/Pages/Dialog/FilesList.cshtml.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace admim.GiamKichSan.Pages.Dialog
+{
+    public class FilesListModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+        public void CreateDirectory()
+		{
+
+		}
+        public void CreateFile()
+		{
+
+		}
+    }
+}
