c4785ec4bef7a8d1f1eb116edda3a73cf722c8ae
LDAP\vcduan <duanvc2811@gmail.com>
2023-01-27 11:11:45 +0700

    add convert

---
+++ Experience/admim.GiamKichSan/wwwroot/js/common/convertHelpers.js
@@ -0,0 +1,12 @@
+﻿let convertHelpers = {
+    toNumber: function (val) {
+        if (val == null)
+            return 0;
+
+        val = val.replaceAll(',', '');
+        if (val == '')
+            return 0;
+        else
+            return Number(val);
+    }
+};
