262ccf2361361a135b63af0c0a932efdf0f2d2ca
LDAP\vcduan <duanvc2811@gmail.com>
2023-02-03 17:23:01 +0700

    sdfs

---
+++ Experience/admim.GiamKichSan/wwwroot/lib/directory-treelist/directory-treelist.js
@@ -0,0 +1,15 @@
+﻿let directoryTreelist = {
+    init: function () {
+        var toggler = document.getElementsByClassName("directory-caret");
+        for (var indexItem = 0; indexItem < toggler.length; indexItem++) {
+            if (toggler[indexItem].getAttribute('cust-event') != 'true') {
+                toggler[indexItem].setAttribute('cust-event', 'true');
+                toggler[indexItem].addEventListener("click", function () {
+                    this.parentElement.querySelector(".nested").classList.toggle("active");
+                    this.classList.toggle("caret-down");
+                });
+            }
+        }
+    }
+};
+directoryTreelist.init();
